a21d2a7a49ec016ea94606cbab7a10235ee0b3c3
Lyova-Web <66631396+Lyova-Web@users.noreply.github.com>
2020-08-16 14:49:19 +0300

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
-      <span class="card-title">Домашняя бухгалтерия</span>
+      <span class="card-title">Авторизация</span>
       <div class="input-field">
         <input  
         id="email"
